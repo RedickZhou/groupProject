a1030c4d3e5428e9bda98d194c248ed22b093bac
RedickZhou <36820406+RedickZhou@users.noreply.github.com>
2018-03-09 13:46:21 -0600

    Update projectJS.js
    update on March 9

--- projectJS.js
+++ projectJS.js
@@ -1,3 +1,15 @@
+var notOfAge = "You must be 21 years of age to drink. No drink specials available.";
+
+function checkAge() {
+var elDrink = document.getElementById('ageChoice2').checked;
+if (elDrink === true){
+    $('#results').append(notOfAge);
+}
+
+};
+
+document.getElementById('submitButton').onclick = checkAge;
+
 function clearResults2() {
     $(function() {
         $('p#results').empty();
@@ -9,13 +21,18 @@ function clearResults2() {
     this.checked = false;
     });
     $('#submitButton').show();
-    $('#results').hide();
 });};
 document.getElementById('resetButton').onclick = clearResults2;
 
+
+
+
+//*********************
 function showDeals1() {
     $(function(){
     $('p#show-week').toggle();
+   
+
 });};
 
 function showDeals2() {
@@ -31,62 +48,118 @@ function showDeals3() {
 function showDeals4() {
     $(function(){
     $('p#show-staff').toggle();
+
 });};
 
 document.getElementById('week').onclick = showDeals1;
 document.getElementById('popular').onclick = showDeals2;
 document.getElementById('best').onclick = showDeals3;
 document.getElementById('staff').onclick = showDeals4;
-//use a switch statement for sidebar, example:
-// status = 'closed'; // set the default menu status
 
-function checkResults(){
-    var styles = {
-        "height": "auto",
-        "border-style": "solid"
-    };
-    $('#results').show().css(styles);
-    $('#submitButton').hide();
-    checkFood();
-    checkAge();
-    checkEvent();
-};
-document.getElementById('submitButton').onclick = checkResults;
 
-var notOfAge = "You must be 21 years of age to drink. No drink specials available.";
 
-function checkAge() {
-    var elDrink = document.getElementById('ageChoice2').checked;
-    if (elDrink === true){
-        $('#results').append(notOfAge);
-    }
-};
+var Foodrecommend1 = "Flacoís Tacos is what we recommend, here is the address 1116 20 West Granville, check http://flacostacosonline.com/ for detial";
 
 function checkFood(){
-var FoodChoice = document.getElementById('activityChoice1').checked;
-var StudentChoice = document.getElementById('studentChoice1').checked;
-    if (FoodChoice === true && StudentChoice === true ) {
-        $('#results').append(Foodrecommend2, Foodrecommend1);
-    }
-    else if (FoodChoice === true && StudentChoice === false) {
-        $('#results').append(Foodrecommend1);
-    }
+    var FoodChoice = document.getElementById('activityChoice1').checked;
+if (FoodChoice === true )
+{
+    $('#results').append(Foodrecommend1);
+    }   
+
 };
 
-var Foodrecommend1 = "Flaco's Tacos is what we recommend, here is the address: 1116-20 West Granville, check their website for details. ";
-//need to show URL as clickable
-var Foodrecommend2 = "Veranda Restaurant & Coffee, it gives 10% off to Loyola students with valid Student ID. ";
 
 
-var Eventrecommend1 = "Men's Volleyball vs Ball State. ";
-var Eventrecommend2 = "Loyola Fine Arts Exibition. ";
-var CommunityEvent = "Mosaic Art Classes Available. ";
 
-function checkEvent() {
-    var eventChoice = document.getElementById('activityChoice3').checked;
-    // var dateChoice = document.getElementById('date').checked;
-    if (eventChoice === true)
+var Foodrecommend2 = "Veranda Restaurant & Coffee, it gives 10% off to loyola student"
+
+function checkLoyolaStudent(){
+        var StudentChoice = document.getElementById('studentChoice1').checked;
+
+        if (StudentChoice === true )
+            if(FoodChoice === true )
+
+            {
+                {
+
+             $('#results').append(Foodrecommend2);
+             $('#results').append(Foodrecommend1);
+                    }
+            }
+};
+
+
+
+
+
+var Eventrecommend1 = "Menís Volleyball vs Ball State. ";
+var Eventrecommend2 = "Loyola Fine Arts Exibition ";
+
+
+function checkevent(){
+var eventChoice = document.getElementById('activityChoice3').checked;
+var dateChoice = document.getElementById('date').checked;
+ 
+ $('#results').append("here is check event");
+
+if (eventChoice === true )
+{
+
+    $('#results').append(Eventrecommend2);
+
+    if (dateChoice === 2018-05-03)
     {
-        $('#results').append(Eventrecommend1, Eventrecommend2, CommunityEvent);
-    }
-};
+    $('#results').append(Eventrecommend1);
+        }
+    }   
+$('#submitButton').hide();
+
+}
+
+
+var count=0;
+var re=new Array("Flaco抯 Tacos",
+	"Veranda Restaurant & Coffee",
+	"Veranda Restaurant & Coffee ， 10% off for student",
+	"Volleyball vs Ball State 7:00 pm Gentile Arena",
+	"In/Motion Dance Film Festival 7:30 pm Newhart Family Theatre",
+	"not recommend");
+function submitButtonClick(){
+	 $('#results').empty();
+	 if(document.getElementById('activityChoice1').checked){
+		 if(document.getElementById('studentChoice1').checked)
+			$('#results').append(re[2]);
+		 else
+			$('#results').append(re[0]);
+		return ;
+	 }
+	 if(document.getElementById('activityChoice3').checked){
+		if(document.getElementById(' date ').value==="2018-05-28") {
+			 $('#results').append(re[3]);
+			 return ;
+		}
+		if(document.getElementById(' date ').value==="2018-06-03") {
+			 $('#results').append(re[4]);
+			 return ;
+		}
+		$('#results').append(re[5]);
+		return ;
+	 }
+	 if(document.getElementById('activityChoice2').checked){
+		 if(document.getElementById('ageChoice1').checked)
+		 {
+			 if(Math.random()*10>5)
+			 	$('#results').append(re[0]);
+			else
+				$('#results').append(re[1]);
+			return ;
+		 }
+	 }
+	 $('#results').append(re[5]);
+}
+document.getElementById('submitButton').onclick = submitButtonClick;
+
+
+
+
